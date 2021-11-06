56e221ef05d28dfa48baee673c1d1aa99b11c623
Liiims <sehee167@naver.com>
2021-11-06 18:46:19 +0900

    회원가입 기능 구현
    MemberDAO - registerMember, emailCheck, nicknameCheck(추후 완성)
    MemberVO   - Serializable 구현 및 생성자 추가
    RegisterController - 회원가입 기능
    email-check-fail/ok - 이메일 중복 확인 팝업창
    register-member - 회원가입 페이지

--- semi-project-moco/src/main/java/org/kosta/moco/controller/MainPageController.java
+++ semi-project-moco/src/main/java/org/kosta/moco/controller/MainPageController.java
@@ -3,7 +3,7 @@ package org.kosta.moco.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class MainpageController implements Controller {
+public class MainPageController implements Controller {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
