71ea7f6b648c25f572f7f66fe7104b2ef5a4fb82
cheoljin408 <kcj409@naver.com>
2021-11-09 10:05:13 +0900

    Update BoardDAO:
    getPostinList(), getTotalPostCount(), updateHits(), getPostingByPostNo() 메소드 추가

--- semi-project-moco/src/main/java/org/kosta/moco/model/BoardDAO.java
+++ semi-project-moco/src/main/java/org/kosta/moco/model/BoardDAO.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import javax.sql.DataSource;
 
@@ -27,5 +28,125 @@ public class BoardDAO {
 		if(rs!=null)
 			rs.close();
 		closeAll(pstmt, con);
+	}
+	public ArrayList<PostVO> getPostingList(PagingBean pagingBean, int languageCode) throws SQLException {
+		ArrayList<PostVO> list = new ArrayList<PostVO>();
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			con = dataSource.getConnection();
+			StringBuilder sql = new StringBuilder();
+			sql.append("select v.rnum, v.post_no, v.post_title, to_char(v.post_regdate, 'yyyy-mm-dd') as post_regdate, m.nickname, v.hits, v.language_code, v.language ");
+			sql.append("from ( ");
+			sql.append("select row_number() over(order by b.post_regdate desc) as rnum, b.post_no, b.post_title, b.post_regdate, b.hits, b.email, b.language_code, l.language ");
+			sql.append("from moco_qna_board b, moco_service_language l ");
+			sql.append("where b.language_code = l.language_code and l.language_code = ? ");
+			sql.append(") v, moco_member m ");
+			sql.append("where v.email = m.email and v.rnum between ? and ? ");
+			sql.append("order by v.post_regdate desc");
+			pstmt = con.prepareStatement(sql.toString());
+			pstmt.setInt(1, languageCode);
+			pstmt.setInt(2, pagingBean.getStartRowNumber());
+			pstmt.setInt(3, pagingBean.getEndRowNumber());
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				PostVO pvo = new PostVO();
+				pvo.setPost_no(rs.getInt(2));
+				pvo.setPost_title(rs.getString(3));
+				pvo.setPost_regdate(rs.getString(4));
+				pvo.setHits(rs.getInt(6));
+				
+				MemberVO mvo = new MemberVO();
+				mvo.setNickname(rs.getString(5));
+				pvo.setMvo(mvo);
+				
+				LanguageVO lvo = new LanguageVO();
+				lvo.setLanguage_code(rs.getInt(7));
+				lvo.setLanguage(rs.getString(8));
+				pvo.setLvo(lvo);
+				
+				list.add(pvo);
+			}
+			
+ 		} finally {
+			closeAll(rs, pstmt, con);
+		}
+		
+		return list;
+	}
+	public int getTotalPostCount(int languageCode) throws SQLException {
+		int totalPostCount = 0;
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			con = dataSource.getConnection();
+			String sql = "select count(*) from moco_qna_board where language_code=?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, languageCode);
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				totalPostCount = rs.getInt(1);
+			}
+		} finally {
+			closeAll(rs, pstmt, con);
+		}
+		
+		return totalPostCount;
+	}
+	public void updateHits(String postNo) throws SQLException {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			con = dataSource.getConnection();
+			String sql = "update moco_qna_board set hits = hits+1 where post_no=?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, postNo);
+			pstmt.executeUpdate();
+		} finally {
+			closeAll(pstmt, con);
+		}
+		
+	}
+	public PostVO getPostingByPostNo(String postNo) throws SQLException {
+		PostVO pvo = null;
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			con = dataSource.getConnection();
+			StringBuilder sql = new StringBuilder();
+			sql.append("select b.post_no, b.post_title, b.post_content, b.post_code, to_char(b.post_regdate, 'yyyy-mm-dd') as post_regdate, b.hits, m.nickname ");
+			sql.append("from moco_qna_board b, moco_member m ");
+			sql.append("where b.email = m.email and b.post_no = ?");
+			pstmt = con.prepareStatement(sql.toString());
+			pstmt.setInt(1, Integer.parseInt(postNo));
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				pvo = new PostVO();
+				pvo.setPost_no(rs.getInt(1));
+				pvo.setPost_title(rs.getString(2));
+				pvo.setPost_content(rs.getString(3));
+				pvo.setPost_code(rs.getString(4));
+				pvo.setPost_regdate(rs.getString(5));
+				pvo.setHits(rs.getInt(6));
+				
+				MemberVO mvo = new MemberVO();
+				mvo.setNickname(rs.getString(7));
+				pvo.setMvo(mvo);
+			}
+		} finally {
+			closeAll(pstmt, con);
+		}
+		
+		return pvo;
 	}
 }
