a4b15a4f1a82e8f333e3e56cecc289fa2ce4c82e
JinA-Kim-hub <zooro09347@gmail.com>
2021-11-09 23:54:55 +0900

    edit-memberinfo.jsp으로 url 넘겨주는 컨트롤러

---
+++ semi-project-moco/src/main/java/org/kosta/moco/controller/EditMyInfoController.java
@@ -0,0 +1,25 @@
+package org.kosta.moco.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.kosta.moco.model.MemberDAO;
+import org.kosta.moco.model.MemberVO;
+
+public class EditMyInfoController implements Controller {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		//로그인 체크
+		HttpSession session = request.getSession(false);
+		if(session==null||session.getAttribute("mvo")==null) {
+			return "redirect:layout.jsp";
+		}
+		
+		request.setAttribute("url", "edit-memberinfo.jsp");
+		return "layout.jsp";
+	}
+
+}
