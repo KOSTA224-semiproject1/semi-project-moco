6d9ba74eb1d9cd31a79007c00827ed81e7020a95
Liiims <sehee167@naver.com>
2021-11-11 11:09:01 +0900

    Create SearchPostListPageController
    - 게시글 검색 컨트롤러

---
+++ semi-project-moco/src/main/java/org/kosta/moco/controller/SearchPostListPageController.java
@@ -0,0 +1,56 @@
+package org.kosta.moco.controller;
+
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.kosta.moco.model.BoardDAO;
+import org.kosta.moco.model.LanguageDAO;
+import org.kosta.moco.model.PagingBean;
+import org.kosta.moco.model.PostVO;
+
+public class SearchPostListPageController implements Controller {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		int languageCode = Integer.parseInt(request.getParameter("languageCode"));
+		request.setAttribute("language", LanguageDAO.getInstance().getLanguageName(languageCode));
+		request.setAttribute("languageCode", languageCode);
+		
+		int totalPostCount = BoardDAO.getInstance().getTotalPostCount(languageCode);
+		String pageNo = request.getParameter("pageNo");
+		PagingBean pagingBean = null;
+	
+		if(pageNo == null) {
+			// 현재 페이지가 1page로 할당되어 있음
+			pagingBean = new PagingBean(totalPostCount);
+		}
+		else {
+			// client에서 보낸 page번호로 할당한다
+			pagingBean = new PagingBean(totalPostCount, Integer.parseInt(pageNo));
+		}
+		
+		/**
+		 * 게시글 검색을 위한 코드
+		 */
+		String field = request.getParameter("f");
+		String query = request.getParameter("q").replaceAll(" ", "");
+		
+		System.out.println("field : " + field);
+		System.out.println("query : " + query);
+
+		ArrayList<PostVO> searchPostList = BoardDAO.getInstance().getSearchPostList(field, query, pagingBean, languageCode);
+		
+		System.out.println("searchPostList : " + searchPostList);
+		
+		request.setAttribute("searchPostList", searchPostList);
+		
+		request.setAttribute("pagingBean", pagingBean);
+		
+		request.setAttribute("url", "board/search.jsp");
+		
+		return "layout.jsp";
+	}
+
+}
